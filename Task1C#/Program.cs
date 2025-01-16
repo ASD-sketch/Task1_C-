51a9e01779c5cd9e6cc1a2524c0b252f12b5ba66
ASD-sketch <ssyahmads@gmail.com>
2025-01-16 16:17:53 +0300

    Add project files.

---
+++ Task1C#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //String name = Console.ReadLine();
+            //Console.WriteLine(name);
+
+            Console.WriteLine("*****************************************");
+
+            //double num1 = 2.5;
+            //String name = "Ahmad";
+            //char ch = 'a';
+            //bool bl = false;
+            //int num = 2;
+            //const int num2 = 3;
+
+
+            string[] car = { "Toyota", "Honda", "Ford"};
+
+            foreach (string c in car)
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine($"Total number of cars: {car.Length}");
+
+        }
+    }
+}
