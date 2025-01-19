85a8f5608e530c60be5c5cac23a3af72a9560c7c
ASD-sketch <ssyahmads@gmail.com>
2025-01-19 09:56:40 +0300

    Task1

--- Task1C#/Program.cs
+++ Task1C#/Program.cs
@@ -23,7 +23,62 @@ namespace Task1C_
             //const int num2 = 3;
 
 
-            string[] car = { "Toyota", "Honda", "Ford"};
+            Console.WriteLine("*****************************************");
+
+            //string[] car = { "Toyota", "Honda", "Ford"};
+
+            //foreach (string c in car)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            //Console.WriteLine($"Total number of cars: {car.Length}");
+
+            Console.WriteLine("*****************************************");
+
+
+            //Console.Write("Input your firstname: ");
+            //string firstName = Console.ReadLine();
+
+            //Console.Write("Input your lastname: ");
+            //string lastName = Console.ReadLine();
+
+            //Console.Write("Input your year of birth: ");
+            //String yearOfBirth = Console.ReadLine();
+
+            //Console.WriteLine($"{firstName} {lastName} {yearOfBirth}");
+
+
+            Console.WriteLine("*****************************************");
+
+
+            //int[] array = new int[10];
+            //Console.WriteLine("Input 10 elements in the array:");
+
+            //array[0] = 1;
+            //array[1] = 1;
+            //array[2] = 2;
+            //array[3] = 3;
+            //array[4] = 4;
+            //array[5] = 5;
+            //array[6] = 6;
+            //array[7] = 7;
+            //array[8] = 8;
+            //array[9] = 9;
+
+
+            //Console.WriteLine("Elements in array are:");
+            //foreach (int element in array)
+            //{
+            //    Console.Write(element + " ");
+            //}
+
+
+
+
+
+
+
+
 
             foreach (string c in car)
             {
