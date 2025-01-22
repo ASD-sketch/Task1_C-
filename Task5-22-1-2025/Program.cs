d1eba88ac66866a186372def8e96f2228a17a18b
ASD-sketch <ssyahmads@gmail.com>
2025-01-22 16:44:28 +0300

    Task5-22-1-2024

---
+++ Task5-22-1-2025/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_22_1_2025
+{
+    class Vehicle
+    {
+        public String Model  { get; set; }
+        public String Brand  { get; set; }
+         
+        public void Start()
+        {
+            Console.WriteLine("Vehicle is starting");
+        }
+    }
+
+    class Car:Vehicle
+    {
+        public int NumberOfDoors { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+        }
+    }
+}
